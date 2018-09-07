c0bd64d97f814ddb599c3d0122a77345a5775e42
xieyangxuejun <xieyangxuejun@gmail.com>
2018-09-07 17:31:14 +0800

    fix java create data dont need input pri id

--- dbstar/src/main/java/cn/foretree/db/star/Constants.kt
+++ dbstar/src/main/java/cn/foretree/db/star/Constants.kt
@@ -3,5 +3,5 @@ package cn.foretree.db.star
 /**
  * Created by silen on 03/09/2018
  */
-const val EDIT_DATABASE_NAME = "edit_data"
+const val EDIT_DATABASE_NAME = "edit_data.db"
 const val EDIT_DATABASE_VERSION = 1
--- dbstar/src/main/java/cn/foretree/db/star/EditData.kt
+++ dbstar/src/main/java/cn/foretree/db/star/EditData.kt
@@ -1,21 +1,28 @@
 package cn.foretree.db.star
 
+import android.arch.persistence.room.ColumnInfo
 import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
+import kotlin.math.E
 
 /**
  * Created by silen on 06/09/2018
  */
 @Entity(tableName = "edit_data")
 data class EditData(
-        @field: SerializedName("user_id")
+        @field: ColumnInfo(name = "user_id")
         val userId: Long,
-        @field: SerializedName("edit_type")
+        @field: ColumnInfo(name = "edit_type")
         val editType: Int,
-        @field: SerializedName("edit_data")
+        @field: ColumnInfo(name = "edit_data")
         val editData: String,
         @field: PrimaryKey(autoGenerate = true)
         val id: Long = 0,
         val timestamp: Long = System.currentTimeMillis()
-)
+) {
+    companion object {
+        fun get(userId: Long, type: Int, data: String): EditData = EditData(userId, type, data)
+        fun empty(userId: Long): EditData = EditData(userId, -1, "")
+    }
+}
